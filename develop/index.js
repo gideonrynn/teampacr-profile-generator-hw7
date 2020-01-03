4a6aa574973feb9fee5295048c35a3de5817b3a5
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-02 21:21:47 -0600

    installed node-html-pdf and ran with example in initiate function

--- develop/index.js
+++ develop/index.js
@@ -2,11 +2,14 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateHTML = require("./generateHTML");
 
+const pdf = require('html-pdf');
+const html = fs.readFileSync('./businesscard.html', 'utf8');
+
 //create questions object with prompts for user
 const questions = [
     {
         type: "input",
-        name: "github",
+        name: "username",
         message: "What is your GitHub username?"
       },
     
@@ -25,8 +28,18 @@ const questions = [
 
 function initiate() {
 
-  //inquirer will prompt user from questions object
-  inquirer.prompt(questions)
+  //inquirer will prompt user from questions object, then using those responses
+  inquirer.prompt(questions).then(({ username, color }) => {
+
+    pdf.create(html).toFile('./businesscard.pdf', function(err, res) {
+
+      if (err) return console.log(err);
+
+      console.log(res); // { filename: '/app/businesscard.pdf' }
+
+    })
+
+  });
 
 }
 
